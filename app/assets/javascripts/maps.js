63e5505ae1229d77518e905a30739e95f3090822
BenjaminCranwell <benjamin.cranwell@yahoo.co.uk>
2014-12-02 16:10:09 +0000

    google pin with radius

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -8,8 +8,39 @@ function initialize() {
       mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+  var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
+
+  var infowindow= new google.maps.InfoWindow({
+    content: contentString
+  });
+
   var marker=new google.maps.Marker({
     position:myCenter,
+  });
+
+  var circle=new google.maps.Circle({
+    map: map,
+    radius: 1000,
+    fillColor: '#AA0000'
+  });
+
+  circle.bindTo('center', marker, 'position');
+
+
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
   });
 
   marker.setMap(map);
