4b73d3c8dbb7a6b201d40cdc253c171438902ce9
ThatTobMate <thale.android@gmail.com>
2015-01-11 14:44:12 +0000

    infowindow form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  respond_to :html, :js
   def index
     @comments = Comment.all
   end
@@ -8,11 +9,20 @@ class CommentsController < ApplicationController
   end
   
   def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.new(:body=>params[:comment])
-    @comment.user_id= current_user.id
-    @comment.save
-    redirect_to @post
+    # binding.pry
+    if params[:post_id] == nil
+      @post = Post.find(params[:comment][:post_id])
+      @comment = @post.comments.new(:body=>params[:comment][:body])
+      @comment.user_id= current_user.id
+      @comment.save
+      respond_with(@comment)
+    else
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.new(:body=>params[:comment])
+      @comment.user_id= current_user.id
+      @comment.save
+      redirect_to @post
+    end
   end
 end
 
